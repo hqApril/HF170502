39085780f5d9823cdfa9f5efa4984ca9c30db929
aprilLLLL <forhyj@126.com>
2017-09-27 12:07:35 +0800

    9.27 12：07

---
+++ framework/libraries/Captcha.class.php
@@ -0,0 +1,84 @@
+<?php
+
+class Captcha {
+        private $charset = 'abcdefghkmnprstuvwxyz23456789';     //验证码中出现的字符
+        private $code;  //当前验证码
+        private $codeLen;   //验证码长度
+        private $width;    //验证码图片宽度
+        private $height;    //验证码图片长度
+        private $font = './123.ttf';
+        private $fontSize;
+        private $fontColor;
+        private $img;
+
+        public function __construct($codeLen, $width, $height) {
+            $this -> codeLen = $codeLen;
+            $this -> width = $width;
+            $this -> height = $height;
+            $this -> fontSize = $height / 2;
+        }
+
+        private function createCode() {
+            $_len = strlen($this -> charset) - 1;
+
+            for ($i = 0; $i < $this -> codeLen; $i++) {
+                $this -> code .= $this -> charset[mt_rand(0, $_len)];
+            }
+        }
+
+        private function createBg() {
+            $this -> img = imagecreatetruecolor($this -> width, $this -> height);
+            $color = imagecolorallocate($this -> img, mt_rand(200, 255), mt_rand(200, 255), mt_rand(200, 255));
+            imagefilledrectangle($this -> img, 0, $this -> height, $this -> width, 0, $color);
+        }
+
+        private function createFont() {
+            $_x = $this -> width / $this -> codeLen;
+
+            for ($i = 0; $i < $this -> codeLen; $i++) {
+                $this -> fontColor = imagecolorallocate($this -> img, mt_rand(0, 100), mt_rand(0, 100), mt_rand(0, 100));
+
+                imagettftext($this -> img, $this -> fontSize, mt_rand(-20, 20), $_x * $i + mt_rand(1, 5), $this -> height / 1.33, $this -> fontColor, $this -> font, $this -> code[$i]);
+            }
+        }
+
+        private function createLine() {
+            for ($i = 0; $i < 6; $i++) {
+                $color = imagecolorallocate($this -> img, mt_rand(0, 156), mt_rand(0, 156), mt_rand(0, 156));
+
+                imageline($this -> img, mt_rand(0, $this -> width), mt_rand(0, $this -> height), mt_rand(0, $this -> width), mt_rand(0, $this -> height), $color);
+            }
+
+            for ($i = 0; $i < 100; $i++) {
+                $color = imagecolorallocate($this -> img, mt_rand(200, 255), mt_rand(200, 255), mt_rand(200, 255));
+
+                imagestring($this -> img, mt_rand(1, 5), mt_rand(0, $this -> width), mt_rand(0, $this -> height), "*", $color);
+            }
+        }
+
+        private function outPut() {
+            header("Content-type:image/png");
+            imagepng($this -> img);
+            imagedestroy($this -> img);
+        }
+
+        public function doImg() {
+           $this -> createBg();
+            $this -> createCode();
+           $this -> createLine();
+           $this -> createFont();
+            $this -> outPut();
+        }
+
+
+
+        public function getCode() {
+            return strtolower($this -> code);
+        }
+    }
+
+    session_start();
+
+    $captcha = new Captcha(4, 100, 40);
+    $captcha -> doImg();
+    $_SESSION['loginCapcha'] = $captcha -> getCode();

ad373e08d896bc186e351ae78331130a696b7c3e
aprilLLLL <forhyj@126.com>
2017-09-27 15:23:02 +0800

    7.27 15:22

--- framework/libraries/Captcha.class.php
+++ framework/libraries/Captcha.class.php
@@ -1,4 +1,5 @@
 <?php
+// 验证码插件
 
 class Captcha {
         private $charset = 'abcdefghkmnprstuvwxyz23456789';     //验证码中出现的字符
@@ -6,11 +7,12 @@ class Captcha {
         private $codeLen;   //验证码长度
         private $width;    //验证码图片宽度
         private $height;    //验证码图片长度
-        private $font = './123.ttf';
-        private $fontSize;
-        private $fontColor;
-        private $img;
+        private $font = './TektonPro.otf';    //字体文件引用
+        private $fontSize;    //字体大小
+        private $fontColor;    //字体颜色
+        private $img;    //图片句柄
 
+        //初始化字符数量、图片长宽、文字大小
         public function __construct($codeLen, $width, $height) {
             $this -> codeLen = $codeLen;
             $this -> width = $width;
@@ -18,6 +20,7 @@ class Captcha {
             $this -> fontSize = $height / 2;
         }
 
+        //创建验证码
         private function createCode() {
             $_len = strlen($this -> charset) - 1;
 
@@ -26,12 +29,14 @@ class Captcha {
             }
         }
 
+        //添加创建背景
         private function createBg() {
             $this -> img = imagecreatetruecolor($this -> width, $this -> height);
             $color = imagecolorallocate($this -> img, mt_rand(200, 255), mt_rand(200, 255), mt_rand(200, 255));
             imagefilledrectangle($this -> img, 0, $this -> height, $this -> width, 0, $color);
         }
 
+        //添加字符
         private function createFont() {
             $_x = $this -> width / $this -> codeLen;
 
@@ -42,6 +47,7 @@ class Captcha {
             }
         }
 
+        //添加线条、雪花
         private function createLine() {
             for ($i = 0; $i < 6; $i++) {
                 $color = imagecolorallocate($this -> img, mt_rand(0, 156), mt_rand(0, 156), mt_rand(0, 156));
@@ -56,22 +62,23 @@ class Captcha {
             }
         }
 
+        //输出图片
         private function outPut() {
             header("Content-type:image/png");
             imagepng($this -> img);
             imagedestroy($this -> img);
         }
 
-        public function doImg() {
-           $this -> createBg();
+        //生成验证码图片
+        public function getImg() {
+            $this -> createBg();
             $this -> createCode();
-           $this -> createLine();
-           $this -> createFont();
+            $this -> createLine();
+            $this -> createFont();
             $this -> outPut();
         }
 
-
-
+        //获取验证码
         public function getCode() {
             return strtolower($this -> code);
         }
@@ -80,5 +87,6 @@ class Captcha {
     session_start();
 
     $captcha = new Captcha(4, 100, 40);
-    $captcha -> doImg();
+    $captcha -> getImg();
     $_SESSION['loginCapcha'] = $captcha -> getCode();
+?>
